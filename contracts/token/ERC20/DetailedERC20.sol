f5c57b8d30685f0a79cbe15ae61ef6ed93b26336
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2021-08-31 00:37:08 -0400

    Update DetailedERC20.sol

--- contracts/token/ERC20/DetailedERC20.sol
+++ contracts/token/ERC20/DetailedERC20.sol
@@ -1,6 +1,8 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.4;
 
-import "./ERC20.sol";
+import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/ERC20.sol";
 
 
 contract DetailedERC20 is ERC20 {
