167bf5787bb4ae3c42da9f12ccd1414d5ace8bd6
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2021-08-31 00:31:57 -0400

    Update RBACMintableToken.sol

--- contracts/token/ERC20/RBACMintableToken.sol
+++ contracts/token/ERC20/RBACMintableToken.sol
@@ -1,7 +1,9 @@
-pragma solidity ^0.4.23;
+// SPDX-License-Identifier: MIT
 
-import "./MintableToken.sol";
-import "../../ownership/rbac/RBAC.sol";
+pragma solidity ^0.8.4;
+
+import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/MintableToken.sol";
+import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/ownership/rbac/RBAC.sol";
 
 
 /**
