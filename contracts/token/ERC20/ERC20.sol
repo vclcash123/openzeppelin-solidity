45ab3d49f21b2c39118a71e597a5c3e1bc433d77
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2021-08-30 23:03:11 -0400

    Update ERC20.sol

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -1,7 +1,8 @@
 pragma solidity ^0.8.4;
 
-import "https://github.com/ConsenSysMesh/openzeppelin-solidity/blob/master/contracts/token/ERC20/BasicToken.sol";
-
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Context.sol";
 
 /**
  * @title ERC20 interface
