63a9e44fab3edb075aa3547b731477c8da0764c5
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2021-08-31 22:42:00 -0400

    Update MintedCrowdsale.sol

--- contracts/crowdsale/emission/MintedCrowdsale.sol
+++ contracts/crowdsale/emission/MintedCrowdsale.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: <SPDX-LICENSED>
+
 pragma solidity ^0.8.4;
 
 import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/crowdsale/Crowdsale.sol";
