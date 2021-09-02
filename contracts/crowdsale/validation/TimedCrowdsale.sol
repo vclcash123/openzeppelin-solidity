8d593884cfd1a4874aee785d1ca2b67ef15a56cf
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2021-09-02 17:52:02 -0400

    Update TimedCrowdsale.sol

--- contracts/crowdsale/validation/TimedCrowdsale.sol
+++ contracts/crowdsale/validation/TimedCrowdsale.sol
@@ -1,11 +1,13 @@
-pragma solidity ^0.8.4;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.soll";
 import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/crowdsale/Crowdsale.sol";
 
 
 /**
- * @title TimedCrowdsale
+ * title TimedCrowdsale
  * @dev Crowdsale accepting contributions only within a time frame.
  */
 contract TimedCrowdsale is Crowdsale {
