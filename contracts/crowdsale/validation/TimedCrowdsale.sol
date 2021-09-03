67fb9e7a86cc5b339fe23f03b6bd60b63e738599
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2021-09-03 00:07:29 -0400

    Update TimedCrowdsale.sol

--- contracts/crowdsale/validation/TimedCrowdsale.sol
+++ contracts/crowdsale/validation/TimedCrowdsale.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.7;
 
-import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/SafeMath.sol";
 import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/crowdsale/Crowdsale.sol";
 
 
