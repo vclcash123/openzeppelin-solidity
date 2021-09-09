e45f824f04d6fd8b79b1b92acaf1b41942f05dc8
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2021-09-09 19:31:22 -0400

    Update BasicToken.sol

--- contracts/token/ERC20/BasicToken.sol
+++ contracts/token/ERC20/BasicToken.sol
@@ -3,9 +3,6 @@
 pragma solidity ^0.8.7;
 
 import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/ERC20Basic.sol";
-import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/IERC20.sol";
-import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/IERC20Metadata.sol";
-import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/Context.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
 
