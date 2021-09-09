5dde528171c339cbd902c16480ccfc497fd0c498
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2021-09-09 17:54:43 -0400

    Update StandardToken.sol

--- contracts/token/ERC20/StandardToken.sol
+++ contracts/token/ERC20/StandardToken.sol
@@ -2,11 +2,7 @@
 
 pragma solidity ^0.8.7;
 
-import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/IERC20.sol";
-import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/IERC20Metadata.sol";
 import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/Context.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
-
 
 contract BasicToken {
   /* *This creates an array with all balancesOf */
