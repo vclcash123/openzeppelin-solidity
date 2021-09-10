1ab8acf33557bea79a7eca0e0f4bf6acbeab25f4
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2021-09-09 20:03:38 -0400

    Update Crowdsale.sol

--- contracts/crowdsale/Crowdsale.sol
+++ contracts/crowdsale/Crowdsale.sol
@@ -3,10 +3,7 @@
 pragma solidity ^0.8.7;
 
 import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/Context.sol";
-
-
-
+import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/SafeMath.sol";
 
 /**
  * @title Crowdsale
