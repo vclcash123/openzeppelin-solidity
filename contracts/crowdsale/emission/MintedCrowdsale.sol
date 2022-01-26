e8eb86e195887f1e4f5dea3c924d5325f35af2c9
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2022-01-25 21:08:30 -0500

    Update MintedCrowdsale.sol

--- contracts/crowdsale/emission/MintedCrowdsale.sol
+++ contracts/crowdsale/emission/MintedCrowdsale.sol
@@ -1,7 +1,9 @@
-pragma solidity ^0.4.23;
+// SPDX-License-Identifier: MIT
 
-import "../Crowdsale.sol";
-import "../../token/ERC20/MintableToken.sol";
+pragma solidity ^0.8.0;
+
+import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/token/ERC20/MintableToken.sol";
 
 
 /**
