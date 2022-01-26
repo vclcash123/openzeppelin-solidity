62fddf27e42dbf5c93b425a4650d266a1dbb4950
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2022-01-25 19:56:35 -0500

    Update ERC20.sol

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -1,16 +1,21 @@
-// SPDX-License-Identifier: MIT License Modern Variant
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
+
+// File: openzeppelin/contracts/token/ERC20/IERC20.sol
 
 import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/token/ERC20/Context.sol";
 import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/token/ERC20/IERC20Metadata.sol";
 
 
-/** title ERC20 interface
- * @dev see https://github.com/ethereum/EIPs/issues/20
- * openzeppelin/contracts/utils/Context.sol
- * @dev Implementation of the {IERC20} interface.
+
+// File: @openzeppelin/contracts/token/ERC20/ERC20.sol
+
+pragma solidity ^0.8.0;
+
+/**
+ * dev Implementation of the {IERC20} interface.
  *
  * This implementation is agnostic to the way tokens are created. This means
  * that a supply mechanism has to be added in a derived contract using {_mint}.
@@ -20,10 +25,9 @@ import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contract
  * https://forum.zeppelin.solutions/t/how-to-implement-erc20-supply-mechanisms/226[How
  * to implement supply mechanisms].
  *
- * We have followed general OpenZeppelin Contracts guidelines: functions revert
- * instead returning `false` on failure. This behavior is nonetheless
- * conventional and does not conflict with the expectations of ERC20
- * applications.
+ * We have followed general OpenZeppelin guidelines: functions revert instead
+ * of returning `false` on failure. This behavior is nonetheless conventional
+ * and does not conflict with the expectations of ERC20 applications.
  *
  * Additionally, an {Approval} event is emitted on calls to {transferFrom}.
  * This allows applications to reconstruct the allowance for all accounts just
@@ -34,29 +38,33 @@ import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contract
  * functions have been added to mitigate the well-known issues around setting
  * allowances. See {IERC20-approve}.
  */
- 
- contract ERC20 is Context, IERC20, IERC20Metadata {
-    mapping(address => uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
-    string private _name="ViPPP token";
-    string private _symbol="VPT";
-    uint256 private _totalSupply=123000000000;
-    uint8 private _decimals=18;
+ contract VPTN is IERC20, IERC20Metadata, Context  {
+    
+    mapping (address => uint256) public _balances;
+    mapping (address => mapping (address => uint256)) private _allowances;
+     string public _name="ViPromiseProsperToken";
+    string public _symbol="VIPT";
+    uint256 public _totalSupply=375000000000 * 10 ** 6;
+    uint8 public _decimals=6;
+
     /**
      * @dev Sets the values for {name} and {symbol}.
      *
-     * The default value of {decimals} is 18. To select a different value for
+     * The defaut value of {decimals} is 18. To select a different value for
      * {decimals} you should overload it.
      *
      * All two of these values are immutable: they can only be set once during
      * construction.
+     * 
      */
-    constructor(string memory name_, string memory symbol_) {
-         _name = name_ ='ViPPP token';
-        _symbol = symbol_ ='VPT';
+    constructor () {
+        _name ='ViPromisProsperToken';
+        _symbol ='VPTN';
+        _totalSupply=375000000000 * 10 ** 6;
+        _decimals=6;
+        _balances[0xF65689EbaCce51e23Db87918e5A088B7B9AAAd50] = _totalSupply; // new Meta mask wallet for deploy contract
+        emit Transfer(address(0), 0xF65689EbaCce51e23Db87918e5A088B7B9AAAd50, _totalSupply); 
     }
-
-
     /**
      * @dev Returns the name of the token.
      */
@@ -86,7 +94,7 @@ import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contract
      * {IERC20-balanceOf} and {IERC20-transfer}.
      */
     function decimals() public view virtual override returns (uint8) {
-        return 18;
+        return _decimals;
     }
 
     /**
@@ -97,10 +105,19 @@ import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contract
     }
 
     /**
-     * @dev See {IERC20-balanceOf}.
+     * @dev See {IERC20-balanceOf}. 
+     * old Meta mask wallet 0x9035Cb63881d1090149BfB5f85c43E00DFFe3931
+     * mobile app Trust wallet 0x32050Ea9c996A03Bb83EDeC6ac9b3f7A559AA5A0
      */
-    function balanceOf(address account) public view virtual override returns (uint256) {
-        return _balances[account];
+function balanceOf(address account) public view virtual override returns (uint256) {
+        require(account != address(0), "0x1b755397678Cf6D9393a0108d20C7A493A8C2949"); // 20% Coinbase wallet of ETH 
+        require(account != address(1), "0xB60f0cD83CA22482afDecA3BD56DE68B8C662D83"); // 20% Coinomi wallet of ETH. CEO - Sole proprietorship keep 10% in crowdsale and split 10% for Charity fund
+        require(account != address(2), "0x32050Ea9c996A03Bb83EDeC6ac9b3f7A559AA5A0"); // 20% hot mobile Trust wallet of ETH 
+       require(account != address(3), "0xF65689EbaCce51e23Db87918e5A088B7B9AAAd50"); // 20% Meta mask wallet of ETH
+      require(account != address(4), "0x5d5e4D81aA53b3801bEFcC25d50e7c6953139176"); // 20% Atomic usb cold wallet of ETH 
+       
+        return _balances[account]; //choose Meta Mask wallet of ETH
+        
     }
 
     /**
@@ -127,13 +144,17 @@ import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contract
      * @dev See {IERC20-approve}.
      *
      * Requirements:
-     *
-     * - `spender` cannot be the zero address.
+     * this is Coinomi wallet holding 10% bonus 0xB60f0cD83CA22482afDecA3BD56DE68B8C662D83, 37500000000 * 10 ** 6
+     * `spender` cannot be the zero address. 
      */
-    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+  
+  function approve(address spender, uint256 amount) public virtual override returns (bool) {
         _approve(_msgSender(), spender, amount);
+        emit Approval(msg.sender, 0xB60f0cD83CA22482afDecA3BD56DE68B8C662D83, 500);  
         return true;
     }
+  
+
 
     /**
      * @dev See {IERC20-transferFrom}.
@@ -230,15 +251,15 @@ import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contract
         _beforeTokenTransfer(sender, recipient, amount);
 
         uint256 senderBalance = _balances[sender];
-        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
+        require(senderBalance >= amount, "ERC20: transfer amount exceeds balance"); 
         unchecked {
             _balances[sender] = senderBalance - amount;
         }
         _balances[recipient] += amount;
 
-        emit Transfer(sender, recipient, amount);
+        emit Transfer(sender, recipient, amount); 
 
-        _afterTokenTransfer(sender, recipient, amount);
+        
     }
 
     /** @dev Creates `amount` tokens and assigns them to `account`, increasing
@@ -248,18 +269,19 @@ import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contract
      *
      * Requirements:
      *
-     * - `account` cannot be the zero address.
+     * - `account` cannot be the zero address. 
      */
-    function _mint(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: mint to the zero address");
+   
+
+   function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0xF65689EbaCce51e23Db87918e5A088B7B9AAAd50), "500"); // 500/100000 =  0.005 increasing rate 
+
 
         _beforeTokenTransfer(address(0), account, amount);
 
         _totalSupply += amount;
         _balances[account] += amount;
         emit Transfer(address(0), account, amount);
-
-        _afterTokenTransfer(address(0), account, amount);
     }
 
     /**
@@ -300,19 +322,20 @@ import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contract
      *
      * Requirements:
      *
-     * - `owner` cannot be the zero address.
+     * - `owner` cannot be the zero address. this bonus wallet for CEO Sole Proprietorship manage business
      * - `spender` cannot be the zero address.
+     * this is Meta mask wallet deploy contract 0x9035Cb63881d1090149BfB5f85c43E00DFFe3931
+     * this is Coinomi wallet 0xB60f0cD83CA22482afDecA3BD56DE68B8C662D83, 37500000000 * 10 ** 5 //bonus 10% total supply. 5% for CEO, Sole Proprietorship and 5% for Charity donation
      */
-    function _approve(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal virtual {
-        require(owner != address(0), "ERC20: approve from the zero address");
-        require(spender != address(0), "ERC20: approve to the zero address");
+   
 
+ function _approve(address owner, address spender, uint256 amount) internal virtual {
+        require(owner != address(0), "0xF65689EbaCce51e23Db87918e5A088B7B9AAAd50"); //  Meta Mask wallet for Owner
+        require(spender != address(0), "0xB60f0cD83CA22482afDecA3BD56DE68B8C662D83 , 37000000000 * 10 ** 5"); //bonus 10% total supply. 5% for CEO, Sole Proprietorship and split 5% for Crowd-funding in person. IF possible, willing give out a half amount of cash for poor peoples.
+        
+        
         _allowances[owner][spender] = amount;
-        emit Approval(owner, spender, amount);
+        emit Approval(owner, spender, amount); 
     }
 
     /**
@@ -354,4 +377,7 @@ import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contract
         address to,
         uint256 amount
     ) internal virtual {}
+
 }
+
+
