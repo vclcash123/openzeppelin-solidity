cdfc507906c5ce9cdb48cc26fdb8836b62ff3aa3
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2022-01-25 19:38:16 -0500

    Update IERC20Metadata.sol

--- contracts/token/ERC20/IERC20Metadata.sol
+++ contracts/token/ERC20/IERC20Metadata.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0; 
 
-import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/token/ERC20/IERC20.sol";
 
 /**
  * @dev Interface for the optional metadata functions from the ERC20 standard.
