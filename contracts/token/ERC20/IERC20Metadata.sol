2d37f0a8c1ade43c77791fe60f8dc27975546ab3
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2022-01-25 19:37:12 -0500

    Update IERC20Metadata.sol

--- contracts/token/ERC20/IERC20Metadata.sol
+++ contracts/token/ERC20/IERC20Metadata.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.0; 
 
 import "https://github.com/vclcash123/openzeppelin-solidity/blob/patch-2/contracts/token/ERC20/IERC20.sol";
 
