6a07825348ff2d5e14d8b290cb0f01e7cb69a718
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2022-01-27 22:58:01 -0500

    Update IncreasingPriceCrowdsale.sol

--- contracts/crowdsale/price/IncreasingPriceCrowdsale.sol
+++ contracts/crowdsale/price/IncreasingPriceCrowdsale.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.4.23;
+pragma solidity 0.8.0;
 
-import "../validation/TimedCrowdsale.sol";
-import "../../math/SafeMath.sol";
+import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/math/SafeMath.sol";
 
 
 /**
