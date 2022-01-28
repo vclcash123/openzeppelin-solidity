92a5b805d95f707d9de95c0a2a55d8507203200f
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2022-01-27 22:14:47 -0500

    Update TimedCrowdsale.sol

--- contracts/crowdsale/validation/TimedCrowdsale.sol
+++ contracts/crowdsale/validation/TimedCrowdsale.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.0;
 
-// import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/math/SafeMath.sol";
-// import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/math/SafeMath.sol";
+import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/crowdsale/Crowdsale.sol";
 
 
 /**
