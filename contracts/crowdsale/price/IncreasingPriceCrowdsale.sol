9793e42a33b5d9235a4a792ce6df62a840634ab5
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2022-01-27 23:07:14 -0500

    Update IncreasingPriceCrowdsale.sol

--- contracts/crowdsale/price/IncreasingPriceCrowdsale.sol
+++ contracts/crowdsale/price/IncreasingPriceCrowdsale.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity 0.8.0;
 
 import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contracts/crowdsale/validation/TimedCrowdsale.sol";
