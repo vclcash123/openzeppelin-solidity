7c3f921b96dae8c20a77b5f586f19d4d317a3954
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2022-01-27 20:34:14 -0500

    Update Crowdsale.sol

--- contracts/crowdsale/Crowdsale.sol
+++ contracts/crowdsale/Crowdsale.sol
@@ -11,14 +11,12 @@ import "https://github.com/vclcash123/openzeppelin-solidity/blob/master/contract
 
 /**
  * @dev Wrappers over Solidity's arithmetic operations.
- *
  * NOTE: `SafeMath` is no longer needed starting with Solidity 0.8. The compiler
  * now has built in overflow checking.
  */
 library SafeMath {
     /**
      * @dev Returns the addition of two unsigned integers, with an overflow flag.
-     *
      * _Available since v3.4._
      */
     function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
@@ -31,7 +29,6 @@ library SafeMath {
 
     /**
      * @dev Returns the substraction of two unsigned integers, with an overflow flag.
-     *
      * _Available since v3.4._
      */
     function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
@@ -43,7 +40,6 @@ library SafeMath {
 
     /**
      * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
-     *
      * _Available since v3.4._
      */
     function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
@@ -60,7 +56,6 @@ library SafeMath {
 
     /**
      * @dev Returns the division of two unsigned integers, with a division by zero flag.
-     *
      * _Available since v3.4._
      */
     function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
@@ -72,7 +67,6 @@ library SafeMath {
 
     /**
      * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
-     *
      * _Available since v3.4._
      */
     function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
@@ -85,11 +79,8 @@ library SafeMath {
     /**
      * @dev Returns the addition of two unsigned integers, reverting on
      * overflow.
-     *
      * Counterpart to Solidity's `+` operator.
-     *
      * Requirements:
-     *
      * - Addition cannot overflow.
      */
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -99,26 +90,17 @@ library SafeMath {
     /**
      * @dev Returns the subtraction of two unsigned integers, reverting on
      * overflow (when the result is negative).
-     *
      * Counterpart to Solidity's `-` operator.
-     *
-     * Requirements:
-     *
-     * - Subtraction cannot overflow.
+     * Requirements: - Subtraction cannot overflow.
      */
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         return a - b;
     }
 
     /**
-     * @dev Returns the multiplication of two unsigned integers, reverting on
-     * overflow.
-     *
-     * Counterpart to Solidity's `*` operator.
-     *
-     * Requirements:
-     *
-     * - Multiplication cannot overflow.
+     * @dev Returns the multiplication of two unsigned integers, reverting on overflow.
+     * Counterpart to Solidity's `*` operator
+     * Requirements: - Multiplication cannot overflow.
      */
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         return a * b;
@@ -127,12 +109,8 @@ library SafeMath {
     /**
      * @dev Returns the integer division of two unsigned integers, reverting on
      * division by zero. The result is rounded towards zero.
-     *
      * Counterpart to Solidity's `/` operator.
-     *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
+     * Requirements: - The divisor cannot be zero.
      */
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         return a / b;
@@ -146,9 +124,7 @@ library SafeMath {
      * opcode (which leaves remaining gas untouched) while Solidity uses an
      * invalid opcode to revert (consuming all remaining gas).
      *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
+     * Requirements: - The divisor cannot be zero.
      */
     function mod(uint256 a, uint256 b) internal pure returns (uint256) {
         return a % b;
@@ -161,8 +137,7 @@ library SafeMath {
      * CAUTION: This function is deprecated because it requires allocating memory for the error
      * message unnecessarily. For custom revert reasons use {trySub}.
      *
-     * Counterpart to Solidity's `-` operator.
-     *
+     * Counterpart to Solidity's `-` operator
      * Requirements: - Subtraction cannot overflow.
      */
     function sub(
@@ -184,9 +159,7 @@ library SafeMath {
      * `revert` opcode (which leaves remaining gas untouched) while Solidity
      * uses an invalid opcode to revert (consuming all remaining gas).
      *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
+     * Requirements: - The divisor cannot be zero.
      */
     function div(
         uint256 a,
@@ -210,9 +183,7 @@ library SafeMath {
      * opcode (which leaves remaining gas untouched) while Solidity uses an
      * invalid opcode to revert (consuming all remaining gas).
      *
-     * Requirements:
-     *
-     * - The divisor cannot be zero.
+     * Requirements: - The divisor cannot be zero.
      */
     function mod(
         uint256 a,
