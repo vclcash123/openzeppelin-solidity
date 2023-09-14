ca1db6208b6ab8653c155e739314725d60fde23b
VinhPhuLuc <86334635+vclcash123@users.noreply.github.com>
2023-09-13 20:15:08 -0400

    Update SafeMath.sol

--- contracts/token/ERC20/SafeMath.sol
+++ contracts/token/ERC20/SafeMath.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT License Modern Variant
 
-pragma solidity ^0.7.2;
+pragma solidity ^0.8.0;
 
 // CAUTION
 // This version of SafeMath should only be used with Solidity 0.8 or later,
